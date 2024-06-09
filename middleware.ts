e20c43426aa022350f876d0b8de1af2f271ed5cd
Rohan Kumar <rohanvkumarv@gmail.com>
2024-06-09 12:08:22 +0530

    hello ji raj

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/", '/dashboard/preview(.*)', '/dashboard/download(.*)'],
+    publicRoutes: ["/", '/dashboard/preview(.)', '/dashboard/download(.)', '/policy', '/terms'],
     ignoredRoutes: ["/api/uploadthing", "/api/saveproject", "/api/getpreview", "/api/payment", "/api/updatepayment"]
 });
 
